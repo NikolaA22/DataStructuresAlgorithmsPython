4f2291642ce136d97f7d539d163fcd462c084676
NikolaA22 <32622417+NikolaA22@users.noreply.github.com>
2017-11-11 19:16:50 +0100

    Initial commit

---
+++ AVL/App.py
@@ -0,0 +1,21 @@
+
+from AVL.BalancedTree import BalancedTree
+
+tree = BalancedTree()
+
+tree.insert(4)
+tree.insert(2)
+tree.insert(3)
+
+tree.traverseInOrder()
+
+
+tree.insert(100)
+tree.insert(8)
+tree.insert(1)
+tree.insert(5)
+
+
+tree.traverseInOrder()
+
+
